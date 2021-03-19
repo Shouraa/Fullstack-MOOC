a3c49b6243f9a6e8c0261b4381ed38be5b7354ab
Shouraa <shorajafar@gmail.com>
2021-03-19 19:56:04 +0200

    Exercises 5.5.-5.10. done

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import Notification from './components/Notification';
 import blogService from './services/blogs';
 import loginService from './services/login';
+import BlogForm from './components/BlogForm';
+import Togglable from './components/Togglable';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
-
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [url, setUrl] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
@@ -46,6 +44,7 @@ const App = () => {
 
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
       blogService.setToken(user.token);
+
       setUser(user);
       console.log('user from app', user);
       setUsername('');
@@ -61,18 +60,31 @@ const App = () => {
     window.location.reload();
   };
 
-  const addNewBlog = async (event) => {
-    event.preventDefault();
-
-    const newBlog = {
-      title,
-      author,
-      url,
-    };
-
-    const addedBlog = await blogService.create(newBlog);
+  const addNewBlog = async (newBlogObject) => {
+    createNewFormRef.current.toggleVisibility();
+    const addedBlog = await blogService.create(newBlogObject);
     setBlogs(blogs.concat(addedBlog));
     notify(`a new blog ${addedBlog.title} by ${addedBlog.author}`);
+  };
+
+  const removeBlog = async (id) => {
+    try {
+      await blogService.remove(id);
+      setBlogs(blogs.filter((b) => b.id !== id));
+    } catch (exception) {
+      notify('failed to remove the blog');
+    }
+  };
+
+  const handleLikes = async (id, buttonType) => {
+    const blog = blogs.find((n) => n.id === id);
+    const changedBlog =
+      buttonType === 'like'
+        ? { ...blog, likes: blog['likes'] + 1 }
+        : { ...blog, likes: blog['likes'] - 1 };
+
+    const updatedBlog = await blogService.update(changedBlog, id);
+    setBlogs(blogs.map((blog) => (blog.id !== id ? blog : updatedBlog)));
   };
 
   const loginForm = () => (
@@ -99,39 +111,26 @@ const App = () => {
     </form>
   );
 
+  const createNewFormRef = useRef();
+
   const createNewForm = () => (
-    <form onSubmit={addNewBlog}>
-      <div>
-        title:
-        <input
-          type='text'
-          value={title}
-          name='Title'
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author:
-        <input
-          type='text'
-          value={author}
-          name='Author'
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        url:
-        <input
-          type='text'
-          value={url}
-          name='Url'
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type='submit'>create</button>
-    </form>
+    <Togglable buttonLabel='create new blog' ref={createNewFormRef}>
+      <BlogForm createBlog={addNewBlog} />
+    </Togglable>
   );
 
+  const blogList = () =>
+    blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map((blog) => (
+        <Blog
+          key={blog.id}
+          blog={blog}
+          user={user}
+          handleChange={handleLikes}
+          removeBlog={removeBlog}
+        />
+      ));
   return (
     <div>
       <Notification message={errorMessage} />
@@ -154,9 +153,7 @@ const App = () => {
             </button>
           </h4>
           {createNewForm()}
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
+          {blogList()}
         </div>
       )}
     </div>
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,55 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from 'react';
+
+const Blog = ({ blog, user, handleChange, removeBlog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 4,
+    border: 'solid ',
+    marginBottom: 4,
+  };
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const compare = user.name.toLowerCase() === blog.author.toLowerCase();
+
+  const removeStyle = { display: compare ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const handleLike = (event) => {
+    handleChange(blog.id, event.target.value);
+  };
+
+  const handleRemove = () => {
+    if (window.confirm(`Remove '${blog.title}'?`)) {
+      removeBlog(blog.id);
+    }
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>
+          {' '}
+          likes: {blog.likes}{' '}
+          <input type='button' value='like' onClick={handleLike} />{' '}
+          <input type='button' value='dislike' onClick={handleLike} />
+        </p>
+        <p>user: {user.name}</p>
+        <button style={removeStyle} onClick={handleRemove}>
+          remove
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
